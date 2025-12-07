1e62d25c2baf26e1bdeefd7cd9281408ae7be019
Henry  Jolliffe <henryjolliffe@Henrys-MacBook-Pro.local>
2025-12-07 22:56:37 +0000

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      
+      {/* 2. Add the component here at the bottom */}
+      <Analytics />
+    </>
+  );
 }
